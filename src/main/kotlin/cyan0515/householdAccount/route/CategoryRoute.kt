f4db259e7736a8aeccde298ecec27b570490ee8d
cyan515 <ballpen517@yahoo.co.jp>
2024-06-02 23:16:08 +0900

    style: delete unnecessary imports and empty lines

--- src/main/kotlin/cyan0515/householdAccount/route/CategoryRoute.kt
+++ src/main/kotlin/cyan0515/householdAccount/route/CategoryRoute.kt
@@ -1,22 +1,14 @@
 package cyan0515.householdAccount.route
 
 import cyan0515.householdAccount.infrastructure.Categories
-import cyan0515.householdAccount.infrastructure.Users
-import cyan0515.householdAccount.model.user.User
-import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.call
 import io.ktor.server.auth.authenticate
-import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
-import io.ktor.server.routing.post
 import io.ktor.server.routing.route
-import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
-import org.mindrot.jbcrypt.BCrypt
-
 
 fun Route.categoryRoutes() {
     route("/categories") {
@@ -29,6 +21,5 @@ fun Route.categoryRoutes() {
                 call.respond(categories)
             }
         }
-
     }
 }
