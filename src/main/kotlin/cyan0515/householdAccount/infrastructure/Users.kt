c56b9ffb3012fe0e710df1ce99e17cf25bcf13ee
cyan515 <81094959+cyan515@users.noreply.github.com>
2024-06-02 21:49:22 +0900

    fix: Users.userName to Users.name (#21)
    * fix: Users.userName to Users.name
    
    * fix: add a line break on EOF

--- src/main/kotlin/cyan0515/householdAccount/infrastructure/Users.kt
+++ src/main/kotlin/cyan0515/householdAccount/infrastructure/Users.kt
@@ -3,6 +3,6 @@ package cyan0515.householdAccount.infrastructure
 import org.jetbrains.exposed.dao.id.IntIdTable
 
 object Users : IntIdTable() {
-    val userName = varchar("userName", 50).uniqueIndex()
+    val name = varchar("name", 50).uniqueIndex()
     val password = varchar("password", 100)
 }
