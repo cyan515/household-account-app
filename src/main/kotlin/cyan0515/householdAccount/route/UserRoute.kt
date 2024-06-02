0209d0b10dda3fa9709d658dda9a59b9644ae1db
cyan515 <ballpen517@yahoo.co.jp>
2024-06-02 18:31:02 +0900

    refactor: avoid wildcard imports

--- src/main/kotlin/cyan0515/householdAccount/route/UserRoute.kt
+++ src/main/kotlin/cyan0515/householdAccount/route/UserRoute.kt
@@ -1,16 +1,16 @@
 package cyan0515.householdAccount.route
 
-import cyan0515.householdAccount.model.user.User
 import cyan0515.householdAccount.infrastructure.Users
+import cyan0515.householdAccount.model.user.User
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.call
-import io.ktor.server.response.respond
 import io.ktor.server.request.receive
+import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
-import io.ktor.server.routing.route
 import io.ktor.server.routing.post
+import io.ktor.server.routing.route
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.transactions.transaction
-import io.ktor.http.HttpStatusCode
 
 fun Route.userRoutes() {
     route("/users") {
