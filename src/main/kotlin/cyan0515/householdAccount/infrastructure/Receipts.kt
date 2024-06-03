39349f0deab016179a4ded858cd5229fbac63357
cyan515 <ballpen517@yahoo.co.jp>
2024-06-03 10:33:58 +0900

    feat: add receipt repositories

---
+++ src/main/kotlin/cyan0515/householdAccount/infrastructure/Receipts.kt
@@ -0,0 +1,9 @@
+package cyan0515.householdAccount.infrastructure
+
+import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.javatime.datetime
+
+object Receipts : IntIdTable() {
+    val userId = integer("userId").references(Users.id)
+    val timestamp = datetime("timestamp")
+}
