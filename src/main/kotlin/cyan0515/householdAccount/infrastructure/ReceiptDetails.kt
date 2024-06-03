99973f73c5577baee4d482f37291d9f54610c006
cyan515 <ballpen517@yahoo.co.jp>
2024-06-03 18:13:37 +0900

    fix: identifiers of PostgreSQL should be snake_case

--- src/main/kotlin/cyan0515/householdAccount/infrastructure/ReceiptDetails.kt
+++ src/main/kotlin/cyan0515/householdAccount/infrastructure/ReceiptDetails.kt
@@ -3,8 +3,8 @@ package cyan0515.householdAccount.infrastructure
 import org.jetbrains.exposed.dao.id.IntIdTable
 
 object ReceiptDetails : IntIdTable() {
-    val receiptId = integer("receiptId").references(Receipts.id)
-    val categoryId = integer("categoryId").references(Categories.id)
-    val itemName = varchar("itemName", 50)
+    val receiptId = integer("receipt_id").references(Receipts.id)
+    val categoryId = integer("category_id").references(Categories.id)
+    val itemName = varchar("item_name", 50)
     val amount = integer("amount")
 }
