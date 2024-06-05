7a12de71a79aa177bc9fcd89c1edc9bb2354ab6c
cyan515 <ballpen517@yahoo.co.jp>
2024-06-05 09:34:18 +0900

    feat: implements repositories on Exposed DAOs

--- src/main/kotlin/cyan0515/householdAccount/infrastructure/Categories.kt
+++ src/main/kotlin/cyan0515/householdAccount/infrastructure/Categories.kt
@@ -1,7 +1,17 @@
 package cyan0515.householdAccount.infrastructure
 
+import cyan0515.householdAccount.model.category.Category
+import cyan0515.householdAccount.model.category.ICategoryRepository
 import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
 
-object Categories: IntIdTable() {
+object Categories : IntIdTable(), ICategoryRepository {
     val name = varchar("name", 50).uniqueIndex()
+
+    override fun readAll(): List<Category> {
+        return transaction {
+            selectAll().map { Category(it[name]) }
+        }
+    }
 }
--- src/main/kotlin/cyan0515/householdAccount/infrastructure/Users.kt
+++ src/main/kotlin/cyan0515/householdAccount/infrastructure/Users.kt
@@ -1,8 +1,30 @@
 package cyan0515.householdAccount.infrastructure
 
+import cyan0515.householdAccount.model.user.IUserRepository
+import cyan0515.householdAccount.model.user.User
 import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.insertAndGetId
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.transactions.transaction
 
-object Users : IntIdTable() {
+object Users : IntIdTable(), IUserRepository {
     val name = varchar("name", 50).uniqueIndex()
     val password = varchar("password", 100)
+
+    override fun create(user: User): Int {
+        return transaction {
+            insertAndGetId {
+                it[name] = user.userName
+                it[password] = user.password
+            }
+        }.value
+    }
+
+    override fun read(name: String): User? {
+        return transaction {
+            select { Users.name eq name }
+                .singleOrNull()
+                ?.let { User(it[Users.name], it[password]) }
+        }
+    }
 }
