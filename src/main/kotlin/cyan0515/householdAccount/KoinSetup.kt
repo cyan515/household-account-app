e68323d308134daca313e28bf25769f07901952c
cyan515 <ballpen517@yahoo.co.jp>
2024-06-05 09:36:50 +0900

    feat: add koin's setup and actual injections of repositories

---
+++ src/main/kotlin/cyan0515/householdAccount/KoinSetup.kt
@@ -0,0 +1,22 @@
+package cyan0515.householdAccount
+
+import cyan0515.householdAccount.infrastructure.Categories
+import cyan0515.householdAccount.infrastructure.Receipts
+import cyan0515.householdAccount.infrastructure.Users
+import cyan0515.householdAccount.model.category.ICategoryRepository
+import cyan0515.householdAccount.model.receipt.IReceiptRepository
+import cyan0515.householdAccount.model.user.IUserRepository
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import org.koin.ktor.plugin.Koin
+
+fun Application.setupKoin() {
+    val mods = org.koin.dsl.module {
+        single<IUserRepository> { Users }
+        single<ICategoryRepository> { Categories }
+        single<IReceiptRepository> { Receipts }
+    }
+    install(Koin) {
+        modules(mods)
+    }
+}
