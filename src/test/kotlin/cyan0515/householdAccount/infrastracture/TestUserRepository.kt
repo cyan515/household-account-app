c123f75b383fdce6f4f143acd35b4a15d5d480d4
cyan515 <ballpen517@yahoo.co.jp>
2024-06-06 13:03:50 +0900

    test: fix the user repository to encrypt a password

--- src/test/kotlin/cyan0515/householdAccount/infrastracture/TestUserRepository.kt
+++ src/test/kotlin/cyan0515/householdAccount/infrastracture/TestUserRepository.kt
@@ -2,12 +2,15 @@ package cyan0515.householdAccount.infrastracture
 
 import cyan0515.householdAccount.model.user.IUserRepository
 import cyan0515.householdAccount.model.user.User
+import org.mindrot.jbcrypt.BCrypt
 
-object TestUserRepository :IUserRepository{
-    private val content: Map<Int,User> = HashMap()
+object TestUserRepository : IUserRepository {
+    val content: HashMap<Int, User> = HashMap()
     override fun create(user: User): Int {
-        content.plus(user.hashCode() to user)
-        return user.hashCode()
+        val hashedPassword = BCrypt.hashpw(user.password, BCrypt.gensalt())
+        val encryptedUser = User(user.userName, hashedPassword)
+        content[encryptedUser.hashCode()] = encryptedUser
+        return encryptedUser.hashCode()
     }
 
     override fun read(name: String): User? {
